fc5dc90d3eb424c6f7fa541ff8af55ae6c28dfe3
Utkarsh Srivastav <117751171+utkarsh18virat@users.noreply.github.com>
2024-07-27 00:02:49 +0530

    appl1.py

--- appl1.py
+++ appl1.py
@@ -1,53 +1,81 @@
+# Import the required libraries
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 import pickle
-import streamlit as st
 import os
 
-# Load the saved model using a relative path
+# Define the relative path for the dataset and model
+data_path = 'diabetes.csv'
 model_path = 'trained_model.sav'
+
+# Load the diabetes dataset to a pandas DataFrame
+diabetes_dataset = pd.read_csv(data_path)
+
+# Print the first 5 rows of the dataset
+print(diabetes_dataset.head())
+
+# To get the number of rows and columns in the dataset
+print(diabetes_dataset.shape)
+
+# To get the statistical measures of the data
+print(diabetes_dataset.describe())
+
+# To get details of the outcome column
+print(diabetes_dataset['Outcome'].value_counts())
+
+# Separating the data and labels
+X = diabetes_dataset.drop(columns='Outcome', axis=1)
+Y = diabetes_dataset['Outcome']
+
+# To print the independent variables
+print(X)
+
+# To print the outcome variable
+print(Y)
+
+# Split the data into train and test
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
+print(X.shape, X_train.shape, X_test.shape)
+
+# Build the model
+classifier = svm.SVC(kernel='linear')
+
+# Train the support vector Machine Classifier
+classifier.fit(X_train, Y_train)
+
+# Accuracy score on the training data
+X_train_prediction = classifier.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+print('Accuracy score of the training data : ', training_data_accuracy)
+
+# Accuracy score on the test data
+X_test_prediction = classifier.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+print('Accuracy score of the test data : ', test_data_accuracy)
+
+input_data = (5, 166, 72, 19, 175, 25.8, 0.587, 51)
+
+# Change the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# Reshape the array for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+
+prediction = classifier.predict(input_data_reshaped)
+print(prediction)
+
+if prediction[0] == 0:
+    print('The person is not diabetic')
+else:
+    print('The person is diabetic')
+
+# Save the trained model
+with open(model_path, 'wb') as file:
+    pickle.dump(classifier, file)
 
 # Load the saved model
 with open(model_path, 'rb') as file:
     loaded_model = pickle.load(file)
-
-# Create a function for Prediction
-def diabetes_prediction(input_data):
-    # Change the input_data to numpy array
-    input_data_as_numpy_array = np.asarray(input_data)
-
-    # Reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-    
-    prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
-
-    if prediction[0] == 0:
-        return 'The person is not diabetic'
-    else:
-        return 'The person is diabetic'
-  
-def main():
-    # Give a title
-    st.title('Diabetes Prediction Web App')
-    
-    # To get the input data from the user
-    Pregnancies = st.text_input('Number of Pregnancies')
-    Glucose = st.text_input('Glucose Level')
-    BloodPressure = st.text_input('Blood Pressure value')
-    SkinThickness = st.text_input('Skin Thickness value')
-    Insulin = st.text_input('Insulin Level')
-    BMI = st.text_input('BMI value')
-    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-    Age = st.text_input('Age of the Person')
-    
-    # Code for Prediction
-    diagnosis = ''
-    
-    # Create a button for Prediction
-    if st.button('Diabetes Test Result'):
-        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-        
-    st.success(diagnosis)
-    
-if __name__ == '__main__':
-    main()
